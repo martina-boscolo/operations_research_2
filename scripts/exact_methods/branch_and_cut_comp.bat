585ab7cff0cec5ef44909d58503b7ec873a86816
Kairos95 <118349608+Kairos95@users.noreply.github.com>
2025-05-11 17:23:24 +0200

    bc tuning

--- scripts/exact_methods/branch_and_cut_comp.bat
+++ scripts/exact_methods/branch_and_cut_comp.bat
@@ -3,11 +3,7 @@ setlocal enabledelayedexpansion
 
 REM Define constants
 set SEED_START=1
-set SEED_END=5
-set PARAM1_START=0
-set PARAM1_END=2
-set PARAM2_START=0
-set PARAM2_END=1
+set SEED_END=10
 set NODES=200
 set TIMELIMIT=60
 
@@ -22,36 +18,59 @@ if exist logs (
 REM Generate log files
 echo Executing...
 for /l %%s in (%SEED_START%,1,%SEED_END%) do (
-    for /l %%p in (%PARAM1_START%,1,%PARAM1_END%) do (
-        for /l %%q in (%PARAM2_START%,1,%PARAM2_END%) do (
-            echo Running with seed=%%s, param1=%%p, param2=%%q...
-            ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 %%p -param2 %%q -verbose 10 > logs\BC_n%NODES%_seed%%s_param1_%%p_param2_%%q.log
-        )
-    )
+    echo Running with seed=%%s, param1=0, param2=0, param3=0...
+    ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 0 -param2 0 -param3 0 -verbose 0 > logs\BC_n%NODES%_seed%%s_p1_0_p2_0_p3_0.log
+    
+    echo Running with seed=%%s, param1=1, param2=0, param3=0...
+    ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 1 -param2 0 -param3 0 -verbose 0 > logs\BC_n%NODES%_seed%%s_p1_1_p2_0_p3_0.log
+    
+    echo Running with seed=%%s, param1=0, param2=1, param3=0...
+    ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 0 -param2 1 -param3 0 -verbose 0 > logs\BC_n%NODES%_seed%%s_p1_0_p2_1_p3_0.log
+    
+    echo Running with seed=%%s, param1=0, param2=0, param3=1...
+    ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 0 -param2 0 -param3 1 -verbose 0 > logs\BC_n%NODES%_seed%%s_p1_0_p2_0_p3_1.log
+
+    echo Running with seed=%%s, param1=1, param2=1, param3=1...
+    ..\..\build\Release\tsp.exe -method BC -n %NODES% -seed %%s -timelimit %TIMELIMIT% -param1 1 -param2 1 -param3 1 -verbose 0 > logs\BC_n%NODES%_seed%%s_p1_1_p2_1_p3_1.log
 )
 
 REM Create CSV with headers for each parameter combination
-echo seed,> bc_stats.csv
-set "header=seed"
-for /l %%p in (%PARAM1_START%,1,%PARAM1_END%) do (
-    for /l %%q in (%PARAM2_START%,1,%PARAM2_END%) do (
-        set "header=!header!,p1=%%p_p2=%%q"
-    )
-)
-echo !header! >> bc_stats.csv
+echo 5,Base,Only warm-up,Only Frac-SECs,Only Post-heu,All > bc_stats.csv
 
 REM Extract data and populate CSV
 for /l %%s in (%SEED_START%,1,%SEED_END%) do (
     set "line=%%s"
-    for /l %%p in (%PARAM1_START%,1,%PARAM1_END%) do (
-        for /l %%q in (%PARAM2_START%,1,%PARAM2_END%) do (
-            for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_param1_%%p_param2_%%q.log') do (
-                set "time=%%a"
-                if "!time:~-1!"==";" set "time=!time:~0,-1!"
-                set "line=!line!,!time!"
-            )
-        )
+
+    for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_p1_0_p2_0_p3_0.log') do (
+        set "time=%%a"
+        if "!time:~-1!"==";" set "time=!time:~0,-1!"
+        set "line=!line!,!time!"
     )
+
+    for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_p1_1_p2_0_p3_0.log') do (
+        set "time=%%a"
+        if "!time:~-1!"==";" set "time=!time:~0,-1!"
+        set "line=!line!,!time!"
+    )
+
+    for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_p1_0_p2_1_p3_0.log') do (
+        set "time=%%a"
+        if "!time:~-1!"==";" set "time=!time:~0,-1!"
+        set "line=!line!,!time!"
+    )
+
+    for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_p1_0_p2_0_p3_1.log') do (
+        set "time=%%a"
+        if "!time:~-1!"==";" set "time=!time:~0,-1!"
+        set "line=!line!,!time!"
+    )
+
+    for /f "tokens=5 delims=;" %%a in ('findstr /C:"$STAT;BranchAndCut" logs\BC_n%NODES%_seed%%s_p1_1_p2_1_p3_1.log') do (
+        set "time=%%a"
+        if "!time:~-1!"==";" set "time=!time:~0,-1!"
+        set "line=!line!,!time!"
+    )
+
     echo !line! >> bc_stats.csv
 )
 
